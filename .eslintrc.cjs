5eef7968c32a4ae8d2d1395abc4b75f6ab59738e
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-01-16 12:36:54 -0600

    eslint: we don't use cypress

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,15 +4,6 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
   root: true,
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', 'google'],
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'tests/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-      ],
-      extends: ['plugin:cypress/recommended'],
-    },
-  ],
   rules: {
     'object-curly-spacing': ['error', 'always'],
     'require-jsdoc': 'off',
