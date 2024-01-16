d7bf22c17e8945f248b259c78559504cc0c7f7e9
Jacob McSwain <jacob.a.mcswain@gmail.com>
2024-01-16 12:39:52 -0600

    fix formatting

--- src/main.js
+++ src/main.js
@@ -6,6 +6,6 @@ import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
 
 createApp(App)
-    .use(VueReCaptcha, { siteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY })
-    .use(VueSweetalert2)
-    .mount('#app');
+  .use(VueReCaptcha, { siteKey: import.meta.env.VITE_RECAPTCHA_SITE_KEY })
+  .use(VueSweetalert2)
+  .mount('#app');
